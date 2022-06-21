ee7032d14761775e8874d2fc59d7def29df7240b
usman1515 <usmansiddique1515@gmail.com>
2022-06-21 17:10:31 +0500

    created day 1 rtl

---
+++ day1/mux.sv
@@ -0,0 +1,14 @@
+`timescale 1ns/100ps
+
+module mux2x1 #(parameter DATA_WIDTH=8) (
+    input   logic [DATA_WIDTH-1:0]  in_a,
+    input   logic [DATA_WIDTH-1:0]  in_b,
+    input   logic                   sel,
+    output  logic [DATA_WIDTH-1:0]  out
+);
+
+    always @(sel) begin
+        out = (sel)? in_a : in_b;
+    end
+
+endmodule
