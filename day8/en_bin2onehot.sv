04e5ba42b474bb757a4625f3b7e9aa414c7f0c3d
usman1515 <usmansiddique1515@gmail.com>
2022-06-21 20:57:46 +0500

    created day 8 rtl

---
+++ day8/en_bin2onehot.sv
@@ -0,0 +1,10 @@
+`timescale 1ns/100ps
+
+module bin2OneHotEncoder #(parameter BIN_WIDTH=4) (
+    input   logic [BIN_WIDTH-1:0]       in_bin,
+    output  logic [(2**BIN_WIDTH)-1:0]  out_onehot
+);
+
+    assign out_onehot = 1'b1 << in_bin;
+
+endmodule
