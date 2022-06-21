909e4ef1de39b9c45a38f8819a1b17a5a6cde211
usman1515 <usmansiddique1515@gmail.com>
2022-06-21 17:20:55 +0500

    created day 2 rtl

---
+++ day2/dff.sv
@@ -0,0 +1,32 @@
+`timescale 1ns/100ps
+
+module dFlipFlop #(parameter DATA_WIDTH=8) (
+    input   logic                   clk,
+    input   logic                   rst_n,
+    input   logic [DATA_WIDTH-1:0]  in_data,
+    output  logic [DATA_WIDTH-1:0]  out_norst,
+    output  logic [DATA_WIDTH-1:0]  out_syncrst,
+    output  logic [DATA_WIDTH-1:0]  out_asyncrst
+);
+
+    always_ff @(posedge clk) begin : noReset
+        out_norst <= in_data;
+    end
+
+    always_ff @(posedge clk) begin : synchronousReset
+        if (rst_n)
+            out_syncrst <= 'd0;
+        else
+            out_syncrst <= in_data;
+        // out_syncrst <= (rst_n)? 'd0 : in_data; 
+    end
+
+    always_ff @(posedge clk or posedge rst_n) begin : asynchronousReset
+        if (rst_n)
+            out_asyncrst <= 'd0;
+        else
+            out_asyncrst <= in_data;
+        // out_asyncrst <= (rst_n)? 'd0 : in_data;
+    end
+
+endmodule
