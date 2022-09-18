c4ab1aa4be511f55f8f9fa53e3bba6c87b2aabaf
usman1515 <usmansiddique1515@gmail.com>
2022-09-19 00:41:53 +0500

    created day 9 tb

---
+++ day09/tb_gray_bin_encoders.sv
@@ -0,0 +1,33 @@
+`timescale 1ns/100ps
+
+module tb_gray_bin_encoders();
+
+    localparam VECTOR_WIDTH=4;
+    localparam T=5;
+
+    logic [VECTOR_WIDTH-1:0] in_bin;
+    logic [VECTOR_WIDTH-1:0] out_bin;
+    logic [VECTOR_WIDTH-1:0] out_gray;
+
+    top_encoder #(.DATA_WIDTH (VECTOR_WIDTH)) dut_top_encoder(
+        .in_bin   (in_bin   ),
+        .in_gray  (out_gray ),
+        .out_gray (out_gray ),
+        .out_bin  (out_bin  )
+    );
+
+    initial begin
+        for (int i=0; i<(2**VECTOR_WIDTH); i=i+1) begin
+            in_bin = i;
+            #T;
+            $display("| Time: %4d | In_Bin: %b | Out_Gray: %b | In_Gray: %b | Out_Bin: %b |",
+                        $time, in_bin, out_gray, out_gray, out_bin);
+        end
+    end
+
+    initial begin
+        $dumpfile("day09/day09.vcd");
+        $dumpvars(0,tb_gray_bin_encoders);
+    end
+
+endmodule
